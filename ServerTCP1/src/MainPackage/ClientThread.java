2eadc0b739d25df86670d935ea9d3cf0c36224df
L <vasilegroza3@gmail.com>
2016-04-26 10:38:01 +0300

    add server side

---
+++ ServerTCP1/src/MainPackage/ClientThread.java
@@ -0,0 +1,37 @@
+package MainPackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+class ClientThread extends Thread {
+	private Socket socket = null ;
+
+	public ClientThread (Socket socket) { this.socket = socket ; }
+
+	public void run () {
+		try {
+			// Get the request from the input stream: client → server
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(socket.getInputStream()));
+			while(true)
+			{
+				String request = in.readLine();
+				// Send the response to the oputput stream: server → client
+				PrintWriter out = new PrintWriter(socket.getOutputStream());
+				System.out.println(request);
+				String raspuns = "Hello dear " + request + "!";
+				out.println(raspuns);
+				out.flush();
+			}
+		} catch (IOException e) {
+			System.err.println("Communication error... " + e);
+		} finally {
+			try {
+				socket.close(); // or use try-with-resources
+			} catch (IOException e) { System.err.println (e); }
+		}
+	}
+}
